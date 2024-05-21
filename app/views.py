a2dec6a54dbf6f6de1fbe28cd30059e3961830d3
unknown <phamtrangtram@gmail.com>
2024-05-21 15:21:23 +0700

    webdemo

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+# Create your views here.
+
+def home(request):
+  # hiển thị hàng trong data
+  product = Product.objects.all()
+  context={'products':product}
+
+  return render(request,'app/home.html',context)  
+def cart(request):
+  if request.user.is_authenticated:
+    customer = request.user.customer
+    order,created = Order.objects.get_or_create(customer = customer,complete = False)
+    items = order.orderitem_set.all()
+  else:
+    items =  []
+  context={'items':items,'order':order}
+  return render(request,'app/cart.html',context)
+def checkout(request):
+  context={}
+  return render(request,'app/checkout.html',context)
